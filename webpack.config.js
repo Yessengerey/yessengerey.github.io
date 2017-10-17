bed50fa2cb14bd12c859b5c49c9936800c0014b3
Gary <yes.murz@gmail.com>
2017-10-16 21:59:44 -0700

    Change location of budle to /

--- webpack.config.js
+++ webpack.config.js
@@ -1,12 +1,12 @@
 var path = require('path');
 var SRC_DIR = path.join(__dirname, '/client/src');
-var DIST_DIR = path.join(__dirname, '/public/dist');
+// var DIST_DIR = path.join(__dirname, '/public/dist');
 
 module.exports = {
   entry: `${SRC_DIR}/index.jsx`,
   output: {
     filename: 'bundle.js',
-    path: DIST_DIR
+    path: '/'
   },
   module: {
     rules: [
