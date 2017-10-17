88ed915994aea476d7663c499da8593c461c4c7e
Gary <yes.murz@gmail.com>
2017-10-17 12:22:10 -0700

    Add react router

--- client/src/index.jsx
+++ client/src/index.jsx
@@ -1,6 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+import {BrowserRoute as Router, Route, Link} from 'react-router-dom';
+
+import About from './components/about.jsx';
+
 class App extends React.Component {
   constructor(props) {
     super(props);
@@ -13,7 +17,15 @@ class App extends React.Component {
   render() {
     return (
       <div>
-        HELLO WORLD
+        <Router>
+          <div><Link to="/">Home</Link></div>
+          <div><Link to="/about">About</Link></div>
+          <div><Link to="/topics">Topics</Link></div>
+        </Router>
+
+
+        <Route path="/about" component={About}/>
+        
       </div>
     )
   }
