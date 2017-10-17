604c06d3d1e60e2caab2f165f180988da4add2c2
Gary <yes.murz@gmail.com>
2017-10-16 21:23:15 -0700

    Add modules, webpack, modify package json

---
+++ webpack.config.js
@@ -0,0 +1,41 @@
+var path = require('path');
+var SRC_DIR = path.join(__dirname, '/client/src');
+var DIST_DIR = path.join(__dirname, '/public/dist');
+
+module.exports = {
+  entry: `${SRC_DIR}/index.jsx`,
+  output: {
+    filename: 'bundle.js',
+    path: DIST_DIR
+  },
+  module: {
+    rules: [
+      { test: /\.(js|jsx)$/,
+        include: SRC_DIR,
+        exclude: ['node_modules'],
+        use: [
+          { loader: 'babel-loader',
+            options: {
+              presets: ['react', 'es2015']
+            }
+          }
+        ]
+      },
+      {
+        test: /\.css$/,
+        use: [
+          {
+            loader: 'style-loader'
+          },
+          {
+            loader: 'css-loader',
+            query: {
+              modules: true,
+              localIdentName: '[name]__[local]___[hash:base64:5]'
+            }
+          }
+        ]
+      }
+    ]
+  }
+};
