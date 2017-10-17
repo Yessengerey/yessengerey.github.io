a0d2c38a3f5618ea37d9b691f0e3c40b8b1b3709
Gary <yes.murz@gmail.com>
2017-10-16 22:02:18 -0700

    Update location of bundle

--- webpack.config.js
+++ webpack.config.js
@@ -1,12 +1,12 @@
 var path = require('path');
 var SRC_DIR = path.join(__dirname, '/client/src');
-// var DIST_DIR = path.join(__dirname, '/public/dist');
+var DIST_DIR = path.join(__dirname, '/static/dist');
 
 module.exports = {
   entry: `${SRC_DIR}/index.jsx`,
   output: {
     filename: 'bundle.js',
-    path: '/'
+    path: DIST_DIR
   },
   module: {
     rules: [
