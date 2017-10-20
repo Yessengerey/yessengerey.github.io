2d698a72694da4f7de93cfaa7c51546218c2ef51
Gary <yes.murz@gmail.com>
2017-10-20 12:19:34 -0700

    Add firebase and styling to home page

--- client/src/components/home.jsx
+++ client/src/components/home.jsx
@@ -1,11 +1,46 @@
+// React
 import React from 'react';
 
-const Home = (props) => {
-  return (
-    <div>
-      HOME
-    </div>
-  )
+// Material UI
+import Paper from 'material-ui/Paper';
+
+import styles from '../../../styles/home/home.css';
+
+const style = {
+  height: 500,
+  width: 400,
+  margin: 20,
+  textAlign: 'center',
+  display: 'inline-block',
+};
+
+class Home extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      profileImgURL: ''
+    }
+  }
+
+  componentWillMount() {
+    storageRef.child('images/profile_image.jpeg').getDownloadURL().then(url => {
+      this.setState({
+        profileImgURL: url
+      });
+    });
+  }
+
+  render() {
+    return (
+      <div>
+        <div className={styles.home_container}>
+          <div className={styles.profile_image_container} style={{backgroundImage: `url(${this.state.profileImgURL})`}}>
+          </div>
+        </div>
+      </div>
+    )
+  }
+
 }
 
 export default Home;
